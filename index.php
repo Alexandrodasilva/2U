fc95b300d3001fd8a10abe30671e6d6d9da49aa4
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-01-20 11:00:40 -0300

    criei uma um arquivo php para onde irei enviar os arquivos e enviar para o banco, no index fiz o method post para enviar o cadastro e um required para cadastrar as informações, só não fiz em complemento, fica opcional.

--- index.php
+++ index.php
@@ -5,38 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style type="text/css">
+        #tamanhoContainer{
+            width: 500px;
+        }
+    </style>
     <title>Processo Seletivo 2U</title>
 </head>
 <body>
-    <div class="container">
-        <form>
+    <div class="container" id="tamanhoContainer" style="margin-top: 40px">
+        <h4>Teste do processo seletivo Tecnologia 2U 2023</h4>
+        <form method="POST" action="inserir_cadastro.php" style="margin-top: 20px">
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">nome</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <label for="exampleFormControlInput1" class="form-label" >nome</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="nome" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">CEP</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" >
+                <label for="exampleFormControlInput1" class="form-label" >CEP</label>
+                <input type="number" class="form-control" id="exampleFormControlInput1" name="cep" placeholder="EX.: 00000-000" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Endereço</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <label for="exampleFormControlInput1" class="form-label" >Endereço</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="endereco" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Bairro</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <label for="exampleFormControlInput1" class="form-label" >Bairro</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="bairro" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Estado</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <label for="exampleFormControlInput1" class="form-label" >Estado</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="estado" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Municipio</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <label for="exampleFormControlInput1" class="form-label" >Municipio</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="municipio" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">complemento</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <label for="exampleFormControlInput1" class="form-label" >complemento</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="complemento">
+            </div>
+            <div style="text-align: right;">
+                <button type="submit" class="btn btn-success bnt-small">Cadastrar</button>
             </div>
         </form>
     </div>
---
+++ inserir_cadastro.php
@@ -0,0 +1,10 @@
+<?php
+$nome=$_POST["nome"];
+$cep = $_POST["cep"];
+$endereco = $_POST["endereco"];
+$bairro = $_POST["bairro"];
+$estado = $_POST["estado"];
+$municipio = $_POST["municipio"];
+$complemento = $_POST["complemento"];
+echo $nome;
+?>
