d32bad5654fb8bc9b0e5e9f44a7dace1a64e2bb5
alexandro de jesus silva <alexsamdrosilva13@gmail.com>
2023-01-20 21:58:39 -0300

    terminando de fazer api do cep no arquivo js e refatorando uma parte do index

--- index.php
+++ index.php
@@ -17,34 +17,38 @@
 </head>
 <body>
     <div class="container" id="tamanhoContainer" style="margin-top: 40px">
-        <h4>Teste do processo seletivo Tecnologia 2U 2023</h4>
+        <h4>Processo Seletivo Tecnologia 2U 2023</h4>
         <form method="POST" action="inserir_cadastro.php" style="margin-top: 20px">
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label" >nome</label>
+                <label for="exampleFormControlInput1" class="form-label" >Nome:</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" name="nome" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label" >CEP</label>
-                <input type="number" class="form-control" id="exampleFormControlInput1" name="cep" placeholder="EX.: 00000-000" required>
+                <label for="exampleFormControlInput1" class="form-label" >CEP:</label>
+                <input type="number" class="form-control" id="exampleFormControlInput1"
+                     name="cep" 
+                     placeholder="EX.: 00000-000" 
+                     onchange={verificar()}
+                     required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label" >Endereço</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" name="endereco" required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label" >Bairro</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" name="bairro" required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label" >Estado</label>
+                <label for="exampleFormControlInput1" class="form-label" >UF:</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" name="estado" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label" >Municipio</label>
+                <label for="exampleFormControlInput1" class="form-label" >Municipio:</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" name="municipio" required>
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label" >complemento</label>
+                <label for="exampleFormControlInput1" class="form-label" >Bairro:</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="bairro" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label" >Endereço:</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" name="endereco" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label" >Complemento:</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" name="complemento">
             </div>
             <div style="text-align: right;">
@@ -53,6 +57,6 @@
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> 
-    <script src="buscarApi.js"></script>
+    <script src="buscaApi.js"></script>
 </body>
 </html>
